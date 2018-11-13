a80091ae3da6911afdd49e819a684de5ef02ed50
Jet Martin <jetamartin@gmail.com>
2018-11-12 16:00:15 -0800

    First commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+
+// Core nodejs packages
+// const path = require('path');
+const http = require('http');
+
+// "Non-core" nodejs packages
+const express = require('express');
+
+// Create PORT variable for Heroku
+const port = process.env.PORT || 5000
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello from the root");
+})
+
+app.post('/login', (req, res) => {
+  res.send("You are logged in");
+})
+
+app.post('/register', (req, res) => {
+  res.send("You are registered");
+})
+
+app.listen(5000, () => {
+  console.log(`Server is up on port ${port}`);
+});
