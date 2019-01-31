9831927dc1743fe080d9411c606ed0cd7d95c386
Jet Martin <jetamartin@gmail.com>
2019-01-30 21:46:09 -0800

    add mongoose.set('useCreateIndex', true) to eliminate deprecation warning

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,9 @@ var mongoose = require('mongoose');
 // Must tell Mongoose which promise library to use - in this case we are using JS built in Promises
 mongoose.Promise = global.Promise;
 
+// Needed to prevent Deprecation warning to user createIndexes instead of depricated collection.ensureIndex
+mongoose.set('useCreateIndex', true);
+
 // Connect to local DB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/taskIt', { useNewUrlParser: true }); 
 
