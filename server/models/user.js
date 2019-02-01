81c305eb8ca60e06160343a73ca00da0bf6e4c20
Jet Martin <jetamartin@gmail.com>
2019-01-31 16:25:32 -0800

    Update POST /users route to generate token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+// Must use schemas to add on custom methods
+var UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
@@ -31,7 +34,43 @@ var User = mongoose.model('User', {
 			required: true
 		}
 	}]
-	
 });
+
+UserSchema.methods.toJSON = function () {
+	var user = this;
+	// Take mongoose variable user and converting it into regular object that only includes the values that we want
+	var userObject = user.toObject();
+	
+	return _.pick(userObject, ['_id', 'email']);
+};
+
+
+
+
+// Use below to create instance methods
+// Need to use reg function rather than arrow function because arrow functions don't bind a this to it. And access to the specific user is needed.
+UserSchema.methods.generateAuthToken = function ( ) {
+	var user = this;
+	var access = 'auth';
+	
+	// Create the token 
+	var token = jwt.sign(
+		{_id: user._id.toHexString(), access },
+		'abc123'
+	).toString();
+	
+	// Update the users token array
+	// Not using array push method because there are some inconsistencies with how push works across versions of mongoDb and this can create "issues"
+	user.tokens = user.tokens.concat([{access, token}]);
+	// Need to return it so that server can chain onto promise 
+	return user.save().then(() => {
+		// token will be passed as the success  argument for the next then call (in the server)
+		return token;
+	});
+};
+
+
+var User = mongoose.model('User', UserSchema);
+
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
+var _ = require('lodash');
 // bodyParser - converts JSON body in http request into JS object 
-var bodyParser = require('body-parser');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose.js'); 
@@ -9,27 +9,23 @@ var {User} = require('./models/user');
 var app = express();
 app.use(bodyParser.json());
 
-// create application/json parser
-//var jsonParser = bodyParser.json();
-
-// create application/x-www-form-urlencoded parser
-//var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
 const port = process.env.PORT || 3000;
 
 app.set('port', process.env.PORT || 3000);
 
 app.post('/users', (req, res) => {
-	var user = new User({
-		email: req.body.email,
-		password: req.body.password
+	// Use lodash .pick method to ensure we only get the params we need (and avoid any that may have been added maliciously)
+	var body = _.pick(req.body, ['email', 'password' ])
+	// Note because of prior line we can just pass in body because it has object w/email & password
+	var user = new User(body);
+	//Save user record in Db including token[]
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e); 
 	})
-	
-	user.save().then((doc) => {
-		res.send(doc);
-	}, (e) => {
-		res.status(400).send(e);
-	});
 });
 
 app.listen(port, () => {
