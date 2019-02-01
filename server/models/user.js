0833a0608b264b3d77fd4fe3643770c3ec4dcc67
Jet Martin <jetamartin@gmail.com>
2019-02-01 15:39:57 -0800

    Add add GET /users/profile and authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
-// Must use schemas to add on custom methods
+// Must use schemas to add on custom instance and model methods
 var UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
@@ -35,7 +35,8 @@ var UserSchema = new mongoose.Schema({
 		}
 	}]
 });
-
+// The .toJSON method is called behind the scenes when you invoke Express's send method to send user model.
+// Here we are overriding the standard .toJSON method so that we can restrict what values get returned in the http response (i.e., just return id and email)
 UserSchema.methods.toJSON = function () {
 	var user = this;
 	// Take mongoose variable user and converting it into regular object that only includes the values that we want
@@ -47,10 +48,13 @@ UserSchema.methods.toJSON = function () {
 
 
 
-// Use below to create instance methods
-// Need to use reg function rather than arrow function because arrow functions don't bind a this to it. And access to the specific user is needed.
+// Instance method to create auth token and save it as part of the user record (document)
+
 UserSchema.methods.generateAuthToken = function ( ) {
+	// Need to use reg function rather than arrow function because arrow functions don't bind a 'this' to it. And access to the specific user nstance is needed.
+	// 'this' references user instance
 	var user = this;
+	
 	var access = 'auth';
 	
 	// Create the token 
@@ -69,6 +73,25 @@ UserSchema.methods.generateAuthToken = function ( ) {
 	});
 };
 
+// Model method 
+UserSchema.statics.findByToken = function (token) {
+	// 'this' returns the associated model
+	var User = this;
+	var decoded;
+	
+	try {
+		decoded = jwt.verify(token, 'abc123')
+	} catch (e) {
+		return Promise.reject('Authenticaton Failed');	
+	}
+	
+	return User.findOne({
+		'_id': decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	})
+	
+};
 
 var User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose.js'); 
 var {User} = require('./models/user'); 
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 app.use(bodyParser.json());
@@ -13,6 +14,10 @@ const port = process.env.PORT || 3000;
 
 app.set('port', process.env.PORT || 3000);
 
+//**************************************
+// Sign-in
+//**************************************
+
 app.post('/users', (req, res) => {
 	// Use lodash .pick method to ensure we only get the params we need (and avoid any that may have been added maliciously)
 	var body = _.pick(req.body, ['email', 'password' ])
@@ -20,13 +25,26 @@ app.post('/users', (req, res) => {
 	var user = new User(body);
 	//Save user record in Db including token[]
 	user.save().then(() => {
+		// Create token & add to User record
 		return user.generateAuthToken();
 	}).then((token) => {
+		// Send token back in header
+		// x-auth used to create custom header
 		res.header('x-auth', token).send(user);
 	}).catch((e) => {
 		res.status(400).send(e); 
 	})
 });
+
+
+
+//**************************************
+// Get Profile
+//**************************************
+app.get('/users/profile', authenticate, (req, res) => {
+	res.send(req.user);
+})
+
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
