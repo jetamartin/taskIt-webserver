879c68097f883fe912b3313d33905aa05fa99caf
Jet Martin <jetamartin@gmail.com>
2019-02-03 11:19:11 -0800

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+// algorithm bcryptjs use to has password
+const bcrypt = require('bcryptjs');
 
 // Must use schemas to add on custom instance and model methods
 var UserSchema = new mongoose.Schema({
@@ -92,6 +94,21 @@ UserSchema.statics.findByToken = function (token) {
 	})
 	
 };
+// Mongoose Middleware
+UserSchema.pre('save', function(next) {
+	var user = this;
+	// Only want to hash password if it has changed otherwise don't hash it
+	if (user.isModified('password')) {
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+});
 
 var User = mongoose.model('User', UserSchema);
 
